a873aeaac84d78ebda9d4bd27939ff6d35798747
YassineHaddad23 <metacodefreelancer@gmail.com>
2023-08-19 17:29:51 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,21 @@
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - Test function for positive_or_negative
+ *
+ * This function sets an integer 'i' to 0 and then calls the
+ * 'positive_or_negative' function to test its behavior with the case of 0.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i; // Declare an integer variable 'i'
+
+    i = 0; // Initialize 'i' with the value 0
+
+    // Call the 'positive_or_negative' function to test it with 'i'
+    positive_or_negative(i);
+
+    return (0); // Return 0 to indicate successful execution
 }
